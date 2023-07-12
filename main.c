f62f369ed290bc25258ce8a5e18f73c7ac11c536
Grayson Hansard <grayson.hansard@gmail.com>
2023-07-12 09:58:17 -0400

    Move tests

--- main.c
+++ main.c
@@ -18,46 +18,6 @@ bool testHash(void) {
 	return ascii_hash('a') == 0 && ascii_hash('b') == 1 && ascii_hash('C') == 2;
 }
 
-bool testArrayAllocate(void) {
-	array const a = array_allocate(2);
-	a.ptr[0] = 'd';
-	a.ptr[1] = 'e';
-	return a.size == 2 && a.ptr != 0 && a.ptr[0] == 'd' && a.ptr[1] == 'e';
-}
-
-bool testArrayInsertWithinAllocatedSize(void) {
-	array const a = array_allocate(2);
-	array const b = array_insert(a, 'z', 0);
-	array const c = array_insert(b, 'y', 1);
-
-	return a.size == 2 && a.ptr[0] == '\0' && a.ptr[1] == '\0'
-		&& b.ptr[0] == 'z' && b.ptr[1] == '\0'
-		&& c.ptr[0] == 'z' && c.ptr[2] == 'y';
-}
-
-bool testArrayResize(void) {
-	array const a = array_allocate(2);
-	a.ptr[0] = 'a';
-	a.ptr[1] = 'b';
-
-	array const b = array_resize(a, 5); // Assume 2^3 resize requirement
-	b.ptr[2] = 'c';
-	b.ptr[3] = 'd';
-	b.ptr[4] = 'e';
-
-	return b.size == 8 && b.ptr[0] == 'a' && b.ptr[1] == 'b' && b.ptr[2] == 'c'
-		&& b.ptr[3] == 'd' && b.ptr[4] == 'e'&& b.ptr[5] == '\0';
-}
-
-bool testArrayInsertResize(void) {
-	array const a = array_allocate(2);
-	a.ptr[0] = 'a';
-	a.ptr[1] = 'b';
-	array const b = array_insert(a, 'c', 2);
-	return b.size == 8 /* assume 2^3 */ 
-		&& b.ptr[0] == 'a' && b.ptr[1] == 'b' && b.ptr[2] == 'c';
-}
-
 struct test tests[] = {
 	{ "testHash", testHash },
 	{ "arrayAllocate", testArrayAllocate },
