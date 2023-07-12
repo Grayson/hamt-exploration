e002946114db896d2394c7b0a6cbc26c34f630bb
Grayson Hansard <grayson.hansard@gmail.com>
2023-07-11 20:34:27 -0400

    Add basic testing and hashing

--- main.c
+++ main.c
@@ -1,10 +1,37 @@
 #include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 
 int ascii_hash(char c) {
 	return tolower(c) - 'a';
 }
 
+struct test
+{
+	char * const name;
+	bool (*fn)(void);
+};
+
+bool testHash(void) {
+	return ascii_hash('a') == 0 && ascii_hash('b') == 1 && ascii_hash('C') == 2;
+}
+
+struct test tests[] = {
+	{ "testHash", testHash },
+};
+
 int main() {
-	return !(0 != printf("HERE!\n"));
+	int const testCount = sizeof(tests) / sizeof(struct test);
+	int numberOfFailingTests = 0;
+	for (int testIndex = 0; testIndex < testCount; testIndex++) {
+		struct test * const t = &tests[testIndex];
+		bool const result = t->fn();
+		numberOfFailingTests += result == false;
+		printf("%d: %s -> %s\n", testIndex, t->name, result ? "passed" : "failed");
+	}
+	if (numberOfFailingTests > 0) {
+		printf("%d failing tests\n", numberOfFailingTests);
+		return 1;
+	}
+	return 0;
 }
