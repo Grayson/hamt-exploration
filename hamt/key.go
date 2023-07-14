a3aaf9d5a3d8f3747c60c3f05c0fb9f3298561e8
Grayson Hansard <grayson.hansard@gmail.com>
2023-07-13 21:01:02 -0400

    Implement Key
    This abstracts some of the basic maths at work

---
+++ hamt/key.go
@@ -0,0 +1,28 @@
+package hamt
+
+type Hashable interface {
+	Hash() uint8
+}
+
+const MASK_BITS = 3 // 3 bits can index all 8 slots in array nodes
+
+type Key struct {
+	item  Hashable
+	hash  uint8
+	depth uint8
+}
+
+func NewKey(hashable Hashable) Key {
+	return Key{hashable, hashable.Hash(), 0}
+}
+
+func (k *Key) moveToNextLevel() {
+	k.depth++
+}
+
+func (k *Key) currentMaskedValue() uint {
+	bitsToShift := MASK_BITS * k.depth
+	shiftedBits := k.hash >> bitsToShift
+	maskedValue := shiftedBits & 0b111
+	return uint(maskedValue)
+}
---
+++ hamt/key_test.go
@@ -0,0 +1,67 @@
+package hamt
+
+import "testing"
+
+func TestKeyMaskedValues(t *testing.T) {
+	tests := []struct {
+		name  string
+		key   HashableInt
+		depth uint8
+		want  uint
+	}{
+		{
+			"Zero",
+			0,
+			0,
+			0,
+		},
+		{
+			"One",
+			1,
+			0,
+			1,
+		},
+		{
+			"Two",
+			2,
+			0,
+			2,
+		},
+		{
+			"Second level, first value",
+			0b1_000,
+			1,
+			1,
+		},
+		{
+			"Second level, all values set",
+			0b111_000,
+			1,
+			7,
+		},
+		{
+			"Third level, first and second values set",
+			0b11_000_000,
+			2,
+			3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			k := &Key{
+				item:  tt.key,
+				hash:  tt.key.Hash(),
+				depth: tt.depth,
+			}
+			if got := k.currentMaskedValue(); got != tt.want {
+				t.Errorf("Key.currentMaskedValue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+type HashableInt int
+
+func (i HashableInt) Hash() uint8 {
+	return uint8(i)
+}
